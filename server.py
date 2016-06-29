bf73d215da9a46aba5e75489578cd8440f1a5986
Michael Arbogast <marboga@gmail.com>
2016-06-29 07:09:24 -0700

    pagination implemented

--- server.py
+++ server.py
@@ -1,5 +1,6 @@
 from flask import Flask, session, redirect, render_template, request, jsonify
 from mysqlconnection import MySQLConnector
+import math
 
 app = Flask(__name__)
 app.secret_key = "sdghejrehwgafc"
@@ -23,11 +24,32 @@ def description(id):
 
 @app.route('/update')
 def update():
-	query = "SELECT * FROM notes"
+	query = "SELECT * FROM notes LIMIT 5"
 	notes = mysql.query_db(query)
+	numquery = "SELECT COUNT(title) FROM notes"
+	pagecount = mysql.query_db(numquery)
+	pagecount = pagecount[0]['COUNT(title)']
+	pages = []
+	for i in range(0, int(math.ceil(pagecount / 5)) + 1):
+		pages.append(i+1)
 	print "/n/n/n/n/n/n" , notes
-	return render_template('partial.html', notes=notes)
+	return render_template('partial.html', notes=notes, pages=pages)
 
+@app.route('/page/<id>')
+def page(id = 1):
+	data = {
+		'page': (int(id) - 1) * 5
+	}
+	query = "SELECT * FROM notes LIMIT :page, 5"
+	notes = mysql.query_db(query, data)
+	numquery = "SELECT COUNT(title) FROM notes"
+	pagecount = mysql.query_db(numquery)
+	pagecount = pagecount[0]['COUNT(title)']
+	pages = []
+	for i in range(0, int(math.ceil(pagecount / 5))):
+		pages.append(i+1)
+	print "/n/n/n/n/n/n" , notes
+	return render_template('partial.html', notes=notes, pages=pages)
 
 @app.route('/new', methods=['POST'])
 def new():
